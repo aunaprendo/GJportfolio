611f58b8f6a12311e50a7cba2e1f3c8c7bb46fc2
Georgia Jean <236709901+aunaprendo@users.noreply.github.com>
2026-02-24 21:48:39 -0400

    f

---
+++ docs/ccjs/069planetTabs.js
@@ -0,0 +1,8 @@
+
+let tabs = document.querySelectorAll()
+
+Create a variable called tabs and assign it the result of querying for all elements with the role attribute set to tab.
+
+Then, create a variable called panels and assign it the result of querying for all elements with the role attribute set to tabpanel.
+
+Remember that you can use the querySelectorAll method to select multiple elements at once
