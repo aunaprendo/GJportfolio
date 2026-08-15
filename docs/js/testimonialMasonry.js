2856bceab52e4bef5dce03322597367a438adbdb
Georgia Jean Metz <georgiajeanmetz@gmail.com>
2026-08-15 12:08:26 -0400

    Update: Testimonies and Spacing

---
+++ docs/js/testimonialMasonry.js
@@ -0,0 +1,34 @@
+const testimonialGrid = document.querySelector(".testimonial-masonry");
+
+if (testimonialGrid) {
+  const testimonialItems =
+    testimonialGrid.querySelectorAll(".testimonial-link");
+
+  function resizeTestimonial(item) {
+    const gridStyles = getComputedStyle(testimonialGrid);
+    const rowHeight = parseFloat(gridStyles.gridAutoRows);
+    const rowGap = parseFloat(gridStyles.rowGap);
+    const itemHeight = item.getBoundingClientRect().height;
+
+    const rowSpan = Math.ceil(
+      (itemHeight + rowGap) / (rowHeight + rowGap)
+    );
+
+    item.style.gridRowEnd = `span ${rowSpan}`;
+  }
+
+  function resizeTestimonials() {
+    testimonialItems.forEach(resizeTestimonial);
+  }
+
+  const testimonialObserver = new ResizeObserver(() => {
+    requestAnimationFrame(resizeTestimonials);
+  });
+
+  testimonialItems.forEach((item) => {
+    testimonialObserver.observe(item);
+  });
+
+  window.addEventListener("load", resizeTestimonials);
+  window.addEventListener("resize", resizeTestimonials);
+}
