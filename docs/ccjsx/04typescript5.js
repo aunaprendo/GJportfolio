92b3d53d39ad484807d3fa9d33ebc802b551ece7
Georgia Jean Metz <georgiajeanmetz@gmail.com>
2026-08-20 13:37:13 -0400

    Fix: emoji app

--- docs/ccjsx/04typescript5.js
+++ docs/ccjsx/04typescript5.js
@@ -1,40 +1,39 @@
 class Dino {
-    emoji;
-    emojiElement;
-    constructor(emojiElement) {
-        this.emojiElement = emojiElement;
-    }
+  emoji;
+  emojiElement;
+  constructor(emojiElement) {
+    this.emojiElement = emojiElement;
+  }
 }
 class Tyrannosaurus extends Dino {
-    constructor(emojiElement) {
-        super(emojiElement);
-        this.emoji = "🦖";
-    }
-    render() {
-        this.emojiElement.innerText = this.emoji;
-    }
+  constructor(emojiElement) {
+    super(emojiElement);
+    this.emoji = "🦖";
+  }
+  render() {
+    this.emojiElement.innerText = this.emoji;
+  }
 }
 class Sauropod extends Dino {
-    constructor(emojiElement) {
-        super(emojiElement);
-        this.emoji = "🦕";
-    }
-    render() {
-        this.emojiElement.innerText = this.emoji;
-    }
+  constructor(emojiElement) {
+    super(emojiElement);
+    this.emoji = "🦕";
+  }
+  render() {
+    this.emojiElement.innerText = this.emoji;
+  }
 }
 function isSelect(element) {
-    return element instanceof HTMLSelectElement;
+  return element instanceof HTMLSelectElement;
 }
 const DinoEmojiElement = document.querySelector("#Dino-emoji");
 const DinoMap = {
-    Tyrannosaurus: new Tyrannosaurus(DinoEmojiElement),
-    Sauropod: new Sauropod(DinoEmojiElement),
+  Tyrannosaurus: new Tyrannosaurus(DinoEmojiElement),
+  Sauropod: new Sauropod(DinoEmojiElement),
 };
 const selectElement = document.querySelector("#dinosaur");
 selectElement.addEventListener("change", (e) => {
-    if (isSelect(e.target)) {
-        DinoMap[e.target.value].render();
-    }
+  if (isSelect(e.target)) {
+    DinoMap[e.target.value].render();
+  }
 });
-export {};
