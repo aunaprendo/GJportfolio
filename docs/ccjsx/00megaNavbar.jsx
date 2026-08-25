2428b00e31a2d465015f003a1c29f3d59e7431f7
Georgia Jean Metz <georgiajeanmetz@gmail.com>
2026-08-25 15:05:03 -0400

    Fix: font shadow

--- docs/ccjsx/00megaNavbar.jsx
+++ docs/ccjsx/00megaNavbar.jsx
@@ -3,22 +3,57 @@ export const Navbar = () => {
     <nav className="navbar">
       <ul>
         <li className="nav-item">
-          <a href="#">Dashboard</a>
+          <a
+            href="https://www.squirrel365.io"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit Link (opens in a new tab)"
+          >
+            Dashboard
+          </a>
         </li>
         <li className="nav-item">
-          <a href="#">Widgets</a>
+          <a
+            href="https://getsquirrel.co/products/widget"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit Link (opens in a new tab)"
+          >
+            Widgets
+          </a>
         </li>
         <li className="nav-item">
           <button aria-expanded="false">Apps</button>
           <ul className="sub-menu" aria-label="Apps">
             <li>
-              <a href="#"></a>Calendar
+              <a
+                href="https://www.daniconnorwild.com/shop/p/calendar-2024-t6y9b-zwbfm-gmnjc"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit Link (opens in a new tab)"
+              >
+                Calendar
+              </a>
             </li>
             <li>
-              <a href="#"></a>Chat
+              <a
+                href="https://youtu.be/yOfpURd9a6Y?si=JwhD8Y8HboxflGIt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit Link (opens in a new tab)"
+              >
+                Chat
+              </a>
             </li>
             <li>
-              <a href="#"></a>Email
+              <a
+                href="https://www.squirrelmail.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit Link (opens in a new tab)"
+              >
+                Email
+              </a>
             </li>
           </ul>
         </li>
